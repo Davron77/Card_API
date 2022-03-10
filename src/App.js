121c5a1c8bbd15b0d5d1df129c51786d9d5b57be
Davron <davronbekq467@gmail.com>
2022-03-10 17:09:36 +0500

    3. Add Components

--- src/App.js
+++ src/App.js
@@ -1,9 +1,13 @@
 import "./App.css";
+//Import Components
+import Header from "./components/Header";
+import Footer from "./components/Footer";
 
 function App() {
   return (
     <div className="App">
-      <h1>Hello</h1>
+      <Header />
+      <Footer />
     </div>
   );
 }
